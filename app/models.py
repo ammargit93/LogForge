eb2ef1ce0928078cdd2619ccf13db35da5b6b2c9
Ammar Ansari <ammar.ice98@gmail.com>
2026-02-11 19:37:46 +0530

    upload to S3 completed

--- app/models.py
+++ app/models.py
@@ -4,5 +4,9 @@ from tortoise.models import Model
 
 class Log(Model):
     id = fields.IntField(pk=True)
-    message = fields.TextField()
+    log_name = fields.TextField()
+    log_size = fields.IntField()
+    log_s3_key = fields.TextField()
+    log_s3_id = fields.TextField()
     created_at = fields.DatetimeField(auto_now_add=True)
+    
--- app/routes/logs.py
+++ app/routes/logs.py
@@ -1,16 +1,33 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File
 from app.models import Log
+from dotenv import load_dotenv
+import boto3
+import uuid
+import os
 
+load_dotenv()
 router = APIRouter(prefix="/logs", tags=["Logs"])
 
+s3 = boto3.client(
+    "s3",
+    region_name=os.getenv("AWS_REGION", "ap-south-1")
+)
+
+BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
 
 @router.post("/")
-async def create_log(message: str):
-    log = await Log.create(message=message)
-    return {"id": log.id, "message": log.message}
+async def create_log(file: UploadFile = File(...)):
 
+    unique_id = str(uuid.uuid4())
+    s3_key = f"logs/{unique_id}_{file.filename}"
 
-@router.get("/")
-async def list_logs():
-    logs = await Log.all()
-    return logs
+    s3.upload_fileobj(file.file, BUCKET_NAME, s3_key)
+
+    log = await Log.create(
+        log_name=file.filename,
+        log_size=file.size,
+        log_s3_key=s3_key,
+        log_s3_id=unique_id,
+    )
+
+    return log
